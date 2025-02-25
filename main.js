b7768ee1384aa0ae9f25c2348b216ec828c03966
LindaTancini <lindatancini95@gmail.com>
2025-02-25 15:50:30 +0100

    feat: add AND for multiples of 3 and 5

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // Creo un ciclo con numeri che vanno da 1 a 100
 for (let i = 1; i <= 100; i++) {
-  // Multipli di 3 stampati come "Fizz"
-  if (i % 3 === 0) {
+  // Multipli sia di 3 e 5 come "FizzBuzz"
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    // Multipli di 3 stampati come "Fizz"
+  } else if (i % 3 === 0) {
     console.log("Fizz");
     // Multipli di 5 stampati come "Buzz"
   } else if (i % 5 === 0) {
