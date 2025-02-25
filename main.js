3b3dedfe627bac66ab95f9c869ec7588d5de8b0e
LindaTancini <lindatancini95@gmail.com>
2025-02-25 15:05:19 +0100

    feat: add multiples of 5

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ for (let i = 1; i <= 100; i++) {
   // Multipli di 3 stampati come "Fizz"
   if (i % 3 === 0) {
     console.log("Fizz");
+    // Multipli di 5 stampati come "Buzz"
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
